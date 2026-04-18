39cf30f71e81ff8007d6a54db8514392db6bd5cb
nivetha <nivethabe1997@gmail.com>
2026-04-18 23:14:54 +0530

    Round Favicon fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -85,6 +85,17 @@ export const metadata: Metadata = {
     },
   },
 
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      // { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      // { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: "/apple-icon.png",
+  },
+
+  themeColor: "#0B0B0B",
+
 };
 
 export default function RootLayout({
